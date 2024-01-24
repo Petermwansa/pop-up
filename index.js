01779a026ea01072e7cdc13a3862fa3360079605
Petermwansa <mwansapeter800@gmail.com>
2024-01-24 22:49:28 +0300

    Finish up the javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+let popupVar = document.getElementById("popup");
+
+openPopup = () => {
+popupVar.classList.add("open__popup");
+}
+
+closePopup = () => {
+    popupVar.classList.remove("open__popup");
+    }
