f23ba9dc4917656cabe7a6ea7bc3c29efe2b13f9
Petermwansa <mwansapeter800@gmail.com>
2024-01-27 22:04:50 +0300

    Makea few changes to suit my Socials post

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let popupVar = document.getElementById("popup");
 
+// this function will be called whenever the submit buttin is clicked
 openPopup = () => {
 popupVar.classList.add("open__popup");
 }
 
+// this function will be called when the okay button is clicked
 closePopup = () => {
     popupVar.classList.remove("open__popup");
-    }
+}
+
+
